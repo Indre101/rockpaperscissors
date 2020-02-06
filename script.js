becc545e1524ce318713da23fab1b8f94e1983f6
indre101 <zygaityte.indre@gmail.com>
2020-02-06 12:29:50 +0100

    function to random computer choice

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const playerOne = document.querySelector("#player1")
 const playerTwo = document.querySelector("#player2")
+const buttonsValues = []
+
+
+
+
+const randomComputerChoice = () => {
+  console.log(buttonsValues[Math.floor(Math.random() * buttonsValues.length)])
+}
 
 
 const buttons = document.querySelectorAll("button")
 buttons.forEach(button => {
+  buttonsValues.push(button.value)
+
   button.addEventListener("click", () => {
+    randomComputerChoice()
+
     addAClassOfButtonValue(playerOne)
   })
 })
 
 
-
 const addAClassOfButtonValue = (playerOne) => {
-
   playerOne.classList.remove(playerOne.classList[1])
-
   playerOne.classList.add(event.target.value)
-  console.log(playerOne);
 }
