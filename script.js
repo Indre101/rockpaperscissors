8f883db27d7dae9a1c87f0a0c0d8a2f9d88f7e4e
indre101 <zygaityte.indre@gmail.com>
2020-02-06 13:54:42 +0100

    adds score

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const playerOne = document.querySelector("#player1")
 const playerTwo = document.querySelector("#player2")
 const buttonsValues = []
+let playerOnePoint = 0;
+let playerTwoPoint = 0;
+const tieImage = document.querySelector("#draw")
+const loseImage = document.querySelector("#lose")
+const winImage = document.querySelector("#win")
+
+
 
 const randomComputerChoice = () => {
   return buttonsValues[Math.floor(Math.random() * buttonsValues.length)]
@@ -12,10 +19,14 @@ buttons.forEach(button => {
   buttonsValues.push(button.value)
 
   button.addEventListener("click", () => {
+    winImage.classList.add("hidden");
+
     addAClassOfButtonValue(playerTwo, randomComputerChoice())
     addAClassOfButtonValue(playerOne, event.target.value)
     checkTheResult()
   })
+
+  console.log("win loose", checkWhoWon());
 })
 
 
@@ -31,7 +42,6 @@ function checkTheResult() {
 
     console.log("it's a tie");
   } else if (!playerOne.classList.contains(playerTwo.classList[1])) {
-    // console.log("not contains");
     notTie(playerOne, playerTwo)
   }
 
@@ -40,13 +50,31 @@ function checkTheResult() {
 function notTie(playerOneChoice, computer) {
 
   if (playerOneChoice.classList.contains("paper") && playerTwo.classList.contains("rock")) {
+    playerOnePoint++
     console.log("win");
+    winImage.classList.remove("hidden");
+
   } else if (playerOneChoice.classList.contains("scissors") && playerTwo.classList.contains("paper")) {
+    playerOnePoint++
     console.log("win");
+    winImage.classList.remove("hidden");
+
   } else if (playerOneChoice.classList.contains("rock") && playerTwo.classList.contains("scissors")) {
+    playerOnePoint++
     console.log("win");
+    winImage.classList.remove("hidden");
+
   } else {
+    playerTwoPoint++
     console.log("loose");
   }
+}
 
+
+function checkWhoWon() {
+  if (playerOnePoint == 2 || playerTwoPoint == 2) {
+    return true;
+  } else {
+    return false;
+  }
 }
